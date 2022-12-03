b92aa013344d49f1cfb8f92df1c16c93c415036d
Nicolas Tardieu <ntardieu.contact@gmail.com>
2022-12-03 15:54:38 +0100

    feat(_template): starter for resolve daily byte

---
+++ _template.js
@@ -0,0 +1,7 @@
+
+const isOriginalFunction = (string) => {
+    return false
+}
+
+const sequenceOf_ = ""
+console.log(`For my sequenceOf_ "${sequenceOf_}" isOriginalFunction() return : ${isOriginalFunction(sequenceOf_)}`);
